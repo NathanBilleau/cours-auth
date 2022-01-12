9f5aa57c8689f2f37bd176dbc63fade27c35bbc4
NathanBilleau <nbilleau@gmail.com>
2022-01-12 16:47:43 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
-$CLIENT_ID = 'e1b487e936938698de64';
-$CLIENT_SECRET = 'cc8d121a5a7a294bfec2b601d0e77d5f672865b5';
+$CLIENT_ID = '';
+$CLIENT_SECRET = '';
 
 
 echo '<a href="https://github.com/login/oauth/authorize?client_id='.$CLIENT_ID.'">se connecter</a>';
